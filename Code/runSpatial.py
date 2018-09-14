8edc041cfae9d924081edda1b5764ba479d5d5eb
olivares-j <avijer@gmai.com>
2018-09-14 17:13:42 +0200

    Adds capability to read csv directly from pandas

--- Code/runSpatial.py
+++ Code/runSpatial.py
@@ -1,4 +1,4 @@
-#!/usr/local/python-2.7.10/bin/python
+#!/opt/anaconda2-5.2.0/envs/idp/bin/python
 '''
 Copyright 2017 Javier Olivares Romero
 
@@ -19,7 +19,6 @@ This file is part of PyAspidistra.
 '''
 from __future__ import absolute_import, unicode_literals, print_function
 import sys
-#sys.path.insert(0, "/pcdisk/boneym5/jolivares/PyAspidistra/Code")
 import os
 import numpy as np
 import importlib
@@ -36,11 +35,6 @@ from scipy.optimize import brentq
 import scipy.stats as st
 from Functions import Deg2pc,TruncSort,DenNum,RotRadii,fMAP,fCovar,Epsilon,MassRj,MassEps
 
-#########################################################################################
-dir_  = os.path.expanduser('~') +"/PyAspidistra/"
-real  = True
-Ntot  = 10000  # Number of stars if synthetic (real = False)
-
 nargs = len(sys.argv)
 if nargs == 4:
 	na = 0
@@ -50,35 +44,23 @@ model = str(sys.argv[1-na])
 Rcut  = float(sys.argv[2-na])
 exte  = str(sys.argv[3-na])
 
-if exte == "None":
-	dext = "Models"
-	fext = "NoCentre/"
-elif exte == "Ctr":
-	dext = "ModelsCtr"
-	fext = "Centre/"
-elif exte == "Ell":
-	dext = "ModelsEll"
-	fext = "Elliptic/"
-elif exte == "Seg":
-	dext = "ModelsSeg"
-	fext = "Segregated/"
-else :
-	print("Extention not recognised!")
-	print("Available ones are: None, Centre (Ctr), Elliptic (Ell), and Segregated (Seg")
-	sys.exit()
 
-########### Load module ######################
-mod = importlib.import_module(dext+"."+model)
-##############################################
+############ GLOBAL VARIABLES #################################################
+dir_  = "/home/jromero/Repos/PyAspidistra/"  # Specify the full path to the Aspidistra path
+real  = True
+Ntot  = 10000  # Number of stars if synthetic (real = False)
 
-############## Directory of outputs #########################
-if real :
-	dir_out  = dir_+'Analysis/'+fext+model+'_'+str(int(Rcut))
-else:
-	dir_out  = dir_+'Analysis/Synthetic/'+fext+model+'_'+str(int(Rcut))+'_'+str(Ntot)
-	fsyn     = dir_out+'/2-data.csv'
-if not os.path.exists(dir_out): os.mkdir(dir_out)
-################################### MODEL ######################################################
+Dist    = 308
+centre  = [289.10,-16.38]
+
+file_members = dir_+'Data/'+'members.csv' # Put here the name of the members file.
+#"Make sure you have data in right order! (R.A., Dec. Probability, Band,e_band)")
+list_observables = ["ra","dec","probability","G","G_error"]
+
+mag_limit = 19.0
+
+
+#-------------------- MODEL PRIORS -----------------------------------------------
 texp = 100.0 # truncation of exponential prior for exponents
 sexp = 1.0  # scale of  ""      """
 src  = 1.0  # scale of half-cauchy for core radius
@@ -186,17 +168,22 @@ if model == "RGDP":
 	hyp     = np.array([src,texp,sexp])
 
 
-######### parameters of centre ####################
+#------------parameters of centre ----------------------------------
 nameparCtr = ["$\\alpha_c\ \ [^\circ]$","$\\delta_c\ \ [^\circ]$"]
 hypCtr    = np.array([1.0,1.0])
-rngCtr    = [[56.4,57],[23.8,24.4]]
+rngCtr    = [[centre[0]-1.0,centre[0]+1.0],[centre[1]-1.0,centre[1]+1.0]]
 ######### Parameter of luminosity segregation #########
 nameSg    = ["$\kappa$ [pc$\cdot \\rm{mag}^{-1}$]"]
 hypSg     = np.array([0,0.5]) # Uniform prior between hypSg[0] and hypSg[0]+hypSg[1]
 rngSg     = [[-0.6,1.2]]
+
+
 #####################################################
 
-#------- concatenate parameters
+
+############################################################
+
+#------- concatenate parameters--------
 if not exte == "None" :
 	namepar = nameparCtr + namepar
 	hyp     = np.append(hypCtr,hyp)
@@ -207,30 +194,52 @@ if exte == "Seg":
 	hyp     = np.append(hyp,hypSg)
 	rng     = sum([rng,rngSg],[])
 
+#------ Determine dext and fext -----
+
+if exte == "None":
+	dext = "Models"
+	fext = "NoCentre/"
+elif exte == "Ctr":
+	dext = "ModelsCtr"
+	fext = "Centre/"
+elif exte == "Ell":
+	dext = "ModelsEll"
+	fext = "Elliptic/"
+elif exte == "Seg":
+	dext = "ModelsSeg"
+	fext = "Segregated/"
+else :
+	print("Extention not recognised!")
+	print("Available ones are: None, Centre (Ctr), Elliptic (Ell), and Segregated (Seg")
+	sys.exit()
+###############################################################################
+
+
+############## Directory of outputs ##################################################
+if real :
+	dir_out  = dir_+'Analysis/'+fext+model+'_'+str(int(Rcut))
+else:
+	dir_out  = dir_+'Analysis/Synthetic/'+fext+model+'_'+str(int(Rcut))+'_'+str(Ntot)
+	fsyn     = dir_out+'/2-data.csv'
+if not os.path.exists(dir_out): os.mkdir(dir_out)
+#########################################################################################
+
+########### Load module ######################
+mod = importlib.import_module(dext+"."+model)
+##############################################
+
 ##################### DATA ##############################################################
-Dist    = 134.4
-centre  = [56.65,24.13]
 if real :
 	#------- reads data ---------------
-	fdata = dir_+'Data/OnlyTycho.csv'
-	cdtsT = np.array(pd.read_csv(fdata,header=0,sep=',',usecols=[1,2,17,18,32],dtype=np.float64))#K band col=21
-	cdtsT[:,[2, 3,4]] = cdtsT[:,[4,2,3]] # Puts band and uncertainti in lasts columns
-	fdata = dir_+'Data/Members.csv'
-	cdtsD  = np.array(pd.read_csv(fdata,header=0,sep=',',usecols=[0,3,4,7,10],dtype=np.float64)) #K band col=8
-	cdtsD[:,[0,1,2]] = cdtsD[:,[1,2,0]] # reorder
-	print("Make shure to have data in right order! (R.A., Dec. Prob., Band,e_band)")
-	cdts  = np.vstack([cdtsT,cdtsD])
+	df_cdts = pd.read_csv(file_members,usecols=list_observables)
+	df_cdts = df_cdts[list_observables]
 	#---- removes duplicateds --------
-	sumc  = np.sum(cdts[:,:2],axis=1)
-	idx   = np.unique(sumc,return_index=True)[1]
-	cdts  = cdts[idx]
-	sumc  = np.sum(cdts[:,:2],axis=1)
-	if len(sumc) != len(list(set(sumc))):
-		sys.exit("Duplicated entries in Coordinates!")
+	df_cdts = df_cdts.drop_duplicates()
 	##----- Select objects with J band observed and less than 19
-	ido   = np.where(cdts[:,3] <= 20)[0]
-	cdts  = cdts[ido]
-	# print(cdts.shape)
+	ido      = np.where(df_cdts.loc[:,list_observables[3]] > mag_limit)[0]
+	df_cdts  = df_cdts.drop(ido)
+	#---- Transform to numpy array ---------
+	cdts  = df_cdts.values
 else :
 	# -------- Check if file exists -------
 	if os.path.exists(fsyn):
@@ -260,8 +269,10 @@ Module  = mod.Module(cdts,Rcut,hyp,Dist,centre)
 ndim     = len(namepar)
 n_params = len(namepar)
 
-# pymultinest.run(Module.LogLike,Module.Priors, n_params,resume = False, verbose = True,#n_live_points=500,
-# 	outputfiles_basename=dir_out+'/0-',multimodal=True, max_modes=2,sampling_efficiency = 'model')
+if not os.path.isfile(dir_out+'/0-.txt'):
+	pymultinest.run(Module.LogLike,Module.Priors, n_params,resume = False, verbose = True,#n_live_points=500,
+		outputfiles_basename=dir_out+'/0-',multimodal=True, max_modes=2,sampling_efficiency = 'model')
+
 
 # analyse the results
 ana = pymultinest.Analyzer(n_params = n_params, outputfiles_basename=dir_out+'/0-')
@@ -332,7 +343,7 @@ pdf.savefig(bbox_inches='tight')  # saves the current figure into a pdf page
 plt.close()
 
 plt.figure()
-n, bins, patches = plt.hist(theta,50, normed=1, facecolor='green', alpha=0.5)
+n, bins, patches = plt.hist(theta,50,density=True, facecolor='green', alpha=0.5)
 plt.xlabel('Position Angle [radians]')
 plt.ylabel('Density [stars $\cdot$ radians$^{-1}$]')
 pdf.savefig(bbox_inches='tight')  # saves the current figure into a pdf page
@@ -429,7 +440,7 @@ if exte == "Ell" or exte == "Seg":
 		epsrt_up   = brentq(lambda x:kde.integrate_box_1d(0,x)-0.84,a=0.0,b=1.0)
 		qepsrt     = [epsrt_low,epsrt_mode,epsrt_up]
 
-		nert,bins,_ = plt.hist(eps_rt,50, normed=1, range=[0,1], 
+		nert,bins,_ = plt.hist(eps_rt,50, density=True, range=[0,1], 
 						ec="black",histtype='step', linestyle='dashed',label="$\epsilon_{rt}$")
 		plt.vlines(qepsrt,0,kde(qepsrt),colors="grey",alpha=0.5,linestyle="dashed")
 		plt.text(0.6,0.7*np.max(nerc),'$\epsilon_{rt}$=[%0.2f,%0.2f,%0.2f]' % ( tuple(qepsrt) ))
@@ -456,7 +467,7 @@ if model in ["GKing","King","OGKing"]:
 	x          = np.linspace(np.min(Nrt),np.max(Nrt),num=1000)
 	Nrt_mode   = np.array(x[kde(x).argmax()]).reshape((1,))
 	bins       = np.linspace(1e3,1e4,num=50)   
-	nrt,_,_ = plt.hist(Nrt,bins=bins, normed=1,ec="black",histtype='step')
+	nrt,_,_ = plt.hist(Nrt,bins=bins, density=True,ec="black",histtype='step')
 	nrt = np.append(nrt,0)
 	plt.ylabel("Density")
 	plt.xlabel("Number of stars within $r_t$")
@@ -478,7 +489,7 @@ if model in ["GKing","King","OGKing"]:
 	x             = np.linspace(np.min(massrj),np.max(massrj),num=1000)
 	massrj_mode   = np.array(x[kde(x).argmax()]).reshape((1,))
 
-	nmr,_,_       = plt.hist(massrj,bins=bins, normed=1,ec="black",ls="dashed",histtype='step',label="$M_{rt}$")
+	nmr,_,_       = plt.hist(massrj,bins=bins, density=True,ec="black",ls="dashed",histtype='step',label="$M_{rt}$")
 	nmr           = np.append(nmr,0)
 	mass_mode     = np.array([massrj_mode]).reshape((1,1))
 	nms           = np.c_[bins,nmr]
@@ -490,7 +501,7 @@ if model in ["GKing","King","OGKing"]:
 		kde           = st.gaussian_kde(massep)
 		x             = np.linspace(np.min(massep),np.max(massep),num=1000)
 		massep_mode   = np.array(x[kde(x).argmax()]).reshape((1,))
-		nme,_,_       = plt.hist(massep,bins=bins, normed=1,ec="black",histtype='step',label="$M_{\epsilon}$")
+		nme,_,_       = plt.hist(massep,bins=bins, density=True,ec="black",histtype='step',label="$M_{\epsilon}$")
 		nme           = np.append(nme,0)
 		mass_mode     = np.array([massrj_mode,massep_mode]).reshape((1,2))
 		nms           = np.c_[bins,nmr,nme]
